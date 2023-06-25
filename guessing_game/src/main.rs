76fbf5a657c760a4dc63b99c1bd593f727bb70ec
Richard Hyesung O <orichh@gmail.com>
2023-06-25 18:03:06 -0400

    init guessing_game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,38 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // println!("The secret number is: {secret_number}");
+    let mut num_guesses = 0;
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        num_guesses += 1;
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win! It took you {num_guesses} tries.");
+                break;
+            }
+        }
+    }
+}
